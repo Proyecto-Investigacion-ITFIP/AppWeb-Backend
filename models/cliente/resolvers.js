195940517dc885ee696aa6316333c36f18ffe8ea
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-04-29 11:58:12 -0500

    modfificar resolver variable _id cliente

--- models/cliente/resolvers.js
+++ models/cliente/resolvers.js
@@ -1,10 +1,10 @@
-import { ClienteMODEL } from './cliente.js';
+import { ClienteMODEL } from "./cliente.js";
 
-const resolversCliente= {
+const resolversCliente = {
   Query: {
     Clientes: async (parent, args, context) => {
-        const clientes = await ClienteMODEL.find();
-        return clientes;
+      const clientes = await ClienteMODEL.find();
+      return clientes;
     },
     Cliente: async (parent, args) => {
       const cliente = await ClienteMODEL.findOne({ _id: args._id });
@@ -16,38 +16,44 @@ const resolversCliente= {
     crearCliente: async (parent, args) => {
       const clienteCreado = await ClienteMODEL.create({
         nombre: args.nombre,
-        apellido: args.apellido,    
+        apellido: args.apellido,
         email: args.email,
         identificacion: args.identificacion,
         telefono: args.telefono,
         departamento: args.departamento,
-        ciudad: args.ciudad
+        ciudad: args.ciudad,
       });
 
       return clienteCreado;
     },
 
     editarCliente: async (parent, args) => {
-      const clienteEditado = await ClienteMODEL.findByIdAndUpdate(args._id, {
-        nombre: args.nombre,
-        apellido: args.apellido,
-        email: args.email,
-        identificacion: args.identificacion,
-        telefono: args.telefono,
-        departamento: args.departamento,
-        ciudad: args.ciudad
-      },
+      const clienteEditado = await ClienteMODEL.findByIdAndUpdate(
+        args._id,
+        {
+          nombre: args.nombre,
+          apellido: args.apellido,
+          email: args.email,
+          identificacion: args.identificacion,
+          telefono: args.telefono,
+          departamento: args.departamento,
+          ciudad: args.ciudad,
+        },
         { new: true }
       );
-      return clienteEditado;  
+      return clienteEditado;
     },
 
     eliminarCliente: async (parent, args) => {
-      if (Object.keys(args).includes('_id')) {
-        const clienteEliminado = await ClienteMODEL.findOneAndDelete({ _id: args._id });
+      if (Object.keys(args).includes("_id")) {
+        const clienteEliminado = await ClienteMODEL.findOneAndDelete({
+          _id: args._id,
+        });
         return clienteEliminado;
-      } else if (Object.keys(args).includes('email')) {
-        const clienteEliminado = await ClienteMODEL.findOneAndDelete({ email: args.email });
+      } else if (Object.keys(args).includes("email")) {
+        const clienteEliminado = await ClienteMODEL.findOneAndDelete({
+          email: args.email,
+        });
         return clienteEliminado;
       }
     },
